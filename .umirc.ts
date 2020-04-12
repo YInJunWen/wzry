807416ba95fefdf218500efb93ce92161f3dff78
yin <yinjwen@gmail.com>
2020-04-13 00:53:03 +0800

    finish

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  
   nodeModulesTransform: {
     type: 'none',
   },
@@ -13,4 +12,6 @@ export default defineConfig({
   //     "pathRewrite": { "^/api": "" } //记录一下 这个地方的^/api猜测是一个正则 为了防止页面中多人开发请求地址带/api/test 直接将api替换为空 防止/api/api/test出现
   //   }
   // }
+  base: '/wzry/',
+  publicPath: '/wzry/',
 });
